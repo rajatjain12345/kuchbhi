210b91cb957c0d80a0e365cd8af7755822aea1f8
rajatjain12345 <98112898+rajatjain12345@users.noreply.github.com>
2022-01-21 20:43:09 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,95 @@
+
+        <!-- footer section start -->
+        <footer class="xs-footer-sec">
+            <div class="container">
+                <div class="footer-area">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="footer-widget">
+                                <div class="footer-logo">
+                                    <a href="index.html">
+                                        <img src="assets/images/logo1.png" alt="">
+                                    </a>
+                                </div>
+                                <p>
+                                    A better way to present your money using fully featured digital currency and now
+                                    available
+                                    on
+                                </p>
+                            </div>
+                        </div><!-- col end -->
+                        <div class="col-lg-2 offset-lg-1 col-sm-6">
+                            <div class="footer-widget">
+                                <h4 class="widget-title">Company</h4>
+                                <ul>
+                                    <li><a href="#"> About us</a></li>
+                                    <li><a href="#"> Our Services</a></li>
+                                    <li><a href="#"> Timeline</a></li>
+                                    <li><a href="#"> On Media</a></li>
+                                </ul>
+                            </div>
+                        </div><!-- col end -->
+                        <div class="col-lg-2 col-sm-6">
+                            <div class="footer-widget support">
+                                <h4 class="widget-title">Support</h4>
+                                <ul>
+                                    <li><a href="#"> Contact Us</a></li>
+                                    <li><a href="#"> FAQ</a></li>
+                                    <li><a href="#"> Support</a></li>
+                                    <li><a href="#"> Token</a></li>
+                                </ul>
+                            </div>
+                        </div><!-- col end -->
+                        <div class="col-lg-3 offset-lg-1 col-sm-6">
+                            <div class="footer-widget">
+                                <h4 class="widget-title">Contact</h4>
+                                <form method="get" class="widget-subscibe">
+                                    <input type="email" name="email" class="subscribe-email" placeholder="Email">
+                                    <button type="submit" class="subs-btn"><i class="fa fa-paper-plane"></i>
+                                    </button>
+                                </form>
+                                <ul>
+                                    <li>contact@ptb.world</li>
+                                </ul>
+                            </div>
+                        </div><!-- col end -->
+                    </div><!-- row end -->
+                </div><!-- footer area end -->
+
+                <!-- copyright area -->
+                <div class="footer-copyright">
+                    <p>
+                        Copyright © 2021 Apotheosis
+                    </p>
+                </div>
+
+            </div><!-- container end -->
+        </footer>
+
+        <!--back to top start-->
+        <div class="BackTo">
+            <a href="#" class="fa fa-angle-up" aria-hidden="true"></a>
+        </div>
+        <!--back to top end-->
+
+
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/navigation.js"></script>
+        <script src="assets/js/jquery.appear.min.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+        <script src="assets/js/chart.min.js"></script>
+        <script src="assets/js/particles.min.js"></script>
+        <script src="assets/js/smooth-scroling.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/venobox/1.9.3/venobox.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('.venobox').venobox();
+            });
+        </script>
+ </body>
+
+</html>
